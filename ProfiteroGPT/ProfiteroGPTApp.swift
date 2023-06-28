32ddf522e149bbefe0d2d3d1aaeb90741a83294e
rekhindh <rekkala.hindhuja@publicissapient.com>
2023-06-28 20:26:42 +0530

    Initial Commit

---
+++ ProfiteroGPT/ProfiteroGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfiteroGPTApp.swift
+//  ProfiteroGPT
+//
+//  Created by Rekkala Hindhuja on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfiteroGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
